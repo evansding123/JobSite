841eaba0e209fa5ccc0982da12e47f0d1a54c0e6
Evans Ding <evansding@gmail.com>
2021-07-10 11:16:20 -0700

    set up react and webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const App = (props) => {
+
+
+  return(
+    <div>Hello World</div>
+  );
+
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              ["@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  devtool: 'eval-source-map',
+
+};
+
+module.exports = config;
