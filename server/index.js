15bf6ddf2c02444dab05e2af224b0483805d6f63
Evans Ding <evansding@gmail.com>
2021-07-10 11:46:50 -0700

    updated server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001 || process.env.PORT;
+
+
+app.use(express.json());
+app.use(express.static('./dist'));
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
