e5d2bde80c244bdcd81fae767a7815cdf9cc9ecd
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-12 14:16:46 -0700

    Separated calendar in its own component

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,3 @@ import App from '../components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-import Calendar from 'tui-calendar'; /* ES6 */
-
-var calendar = new Calendar('#calendar', {
-  defaultView: 'month',
-  taskView: true,
-  template: {
-    monthDayname: function(dayname) {
-      return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-    }
-  }
-});
