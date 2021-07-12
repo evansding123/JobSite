c0cf44cba0a6740187f21935cd09e98d88125f85
Brandon Parker <brandon.ch.parker@gmail.com>
2021-07-12 13:54:20 -0700

    Created Schema

---
+++ db/schema.sql
@@ -0,0 +1,137 @@
+DROP DATABASE IF EXISTS "job_site";
+CREATE DATABASE "job_site";
+\c "job_site";
+
+DROP TABLE IF EXISTS
+  "accounts, notes, notifications, job_seekers, employers, jobs, job_seekers_jobs";
+
+  CREATE TYPE industry AS ENUM (
+  'Agriculture',
+  'Construction',
+  'Consulting',
+  'Education',
+  'Healthcare',
+  'Hospitality',
+  'Law',
+  'Manufacturing',
+  'Transportation',
+  'Tech',
+  'Utilities',
+  'Other'
+);
+
+CREATE TYPE remote AS ENUM (
+  'Remote',
+  'Onsite',
+  'Mixed'
+);
+
+CREATE TYPE employment_type AS ENUM (
+  'Full Time',
+  'Part Time',
+  'Temporary',
+  'internship'
+);
+
+CREATE TYPE experience AS ENUM (
+  'Entry',
+  'Mid-level',
+  'Senior',
+  'Executive'
+);
+
+CREATE TABLE "accounts" (
+  "id" serial PRIMARY KEY,
+  "email" varchar(255) NOT NULL,
+  "username" varchar(255) NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "location_coordinates" varchar(255),
+  "location_address" varchar(255) NOT NULL
+);
+
+CREATE TABLE "notes" (
+  "note" text NOT NULL,
+  "accounts_id" integer NOT NULL --foregin key
+);
+
+CREATE TABLE "notifications" (
+  "id" serial PRIMARY KEY,
+  "date"  date NOT NULL DEFAULT CURRENT_DATE,
+  "notification" varchar(255),
+  "accounts_id" integer NOT NULL --foregin key
+);
+
+CREATE TABLE "employers" (
+  "id" serial PRIMARY KEY,
+  "accounts_id" integer NOT NULL --foregin key
+);
+
+CREATE TABLE "job_seekers" (
+  "id" serial PRIMARY KEY,
+  "resume" text,
+  "cover_letter" text,
+  "accounts_id" integer NOT NULL --foregin key
+);
+
+CREATE TABLE "jobs" (
+  "id" serial PRIMARY KEY,
+  "industry" industry NOT NULL, --constraints
+  "employment_type" employment_type NOT NULL, --constraints
+  "title" varchar(255) NOT NULL,
+  "salary" integer NOT NULL,
+  "remote" remote NOT NULL,
+  "experience" experience, --constraints
+  "date" date NOT NULL DEFAULT CURRENT_DATE,
+  -- "location_coordinates" varchar(255), --set default based off of accounts
+  -- "location_address" varchar(255) NOT NULL, --set default based off of accounts
+  "employers_id" integer NOT NULL --foreign key
+);
+
+CREATE TABLE "job_seekers_jobs" (
+  "id" serial PRIMARY KEY,
+  "job_seekers_id" integer NOT NULL, --foreign key
+  "jobs_id" integer NOT NULL --foreign key
+);
+
+ALTER TABLE "notes" ADD FOREIGN KEY ("accounts_id") REFERENCES "accounts" ("id");
+ALTER TABLE "notifications" ADD FOREIGN KEY ("accounts_id") REFERENCES "accounts" ("id");
+ALTER TABLE "employers" ADD FOREIGN KEY ("accounts_id") REFERENCES "accounts" ("id");
+ALTER TABLE "job_seekers" ADD FOREIGN KEY ("accounts_id") REFERENCES "accounts" ("id");
+ALTER TABLE "jobs" ADD FOREIGN KEY ("employers_id") REFERENCES "employers" ("id");
+ALTER TABLE "job_seekers_jobs" ADD FOREIGN KEY ("job_seekers_id") REFERENCES "job_seekers" ("id");
+ALTER TABLE "job_seekers_jobs" ADD FOREIGN KEY ("jobs_id") REFERENCES "jobs" ("id");
+
+CREATE INDEX ON "notes" ("accounts_id");
+CREATE INDEX ON "notifications" ("accounts_id");
+CREATE INDEX ON "employers" ("accounts_id");
+CREATE INDEX ON "job_seekers" ("accounts_id");
+CREATE INDEX ON "jobs" ("employers_id");
+CREATE INDEX ON "job_seekers_jobs" ("job_seekers_id");
+CREATE INDEX ON "job_seekers_jobs" ("jobs_id");
+
+-- ALTER TABLE "jobs" ADD CONSTRAINT check_types CHECK (
+--   "industry" = "Agriculture" OR
+--   "industry" = "Construction" OR
+--   "industry" = "Consulting" OR
+--   "industry" = "Education" OR
+--   "industry" = "Healthcare" OR
+--   "industry" = "Hospitality" OR
+--   "industry" = "Law" OR
+--   "industry" = "Manufacturing" OR
+--   "industry" = "Transportation" OR
+--   "industry" = "Tech" OR
+--   "industry" = "Utilities" OR
+--   "industry" = "Other"
+-- );
+
+-- ALTER TABLE "jobs" ADD CONSTRAINT check_types CHECK (
+--   "employment_type" = "Full Time" OR
+--   "employment_type" = "Part Time" OR
+--   "employment_type" = "Temporary"
+-- );
+
+-- ALTER TABLE "jobs" ADD CONSTRAINT check_types CHECK (
+--   "experience" = "Entry" OR
+--   "experience" = "Mid-level" OR
+--   "experience" = "Senior"
+-- );
