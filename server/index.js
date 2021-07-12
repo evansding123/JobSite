7fed65c06cd5a79cffd1eac9cd9b0e2c8a4e60a4
Brandon Parker <brandon.ch.parker@gmail.com>
2021-07-12 14:17:21 -0700

    Setup pool for db, created router middleware variable

--- server/index.js
+++ server/index.js
@@ -3,12 +3,11 @@ const path = require('path');
 
 const app = express();
 const PORT = 3001 || process.env.PORT;
+const router = express.Router();
 
 
 app.use(express.json());
 app.use(express.static('./dist'));
-
-
 
 
 
