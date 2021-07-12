e5e7abf303740f0668658e4e169e2318b61aca33
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-12 15:09:29 -0700

    Added router, models and controller setup

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = 3001 || process.env.PORT;
-const router = express.Router();
-
+const PORT = 3001;
+const router = require('./routes.js');
+const morgan = require('morgan');
 
 
 app.use(express.json());
 app.use(express.static('./dist'));
-
-
-
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+module.exports = app;
--- server/routes.js
+++ server/routes.js
@@ -1,2 +1,6 @@
 const controller = require('./controllers/index.js');
+const router = require('express').Router();
 
+
+
+module.exports = router;
