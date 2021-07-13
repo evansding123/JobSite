1b5fb44ce47fb0f867a59b428c6d5ca5831887ee
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-13 00:35:20 -0700

    Added posting pathway for accounts

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 module.exports.jobs = require('./jobs.js');
 module.exports.notes = require('./notes.js');
 module.exports.notifications = require('./notifications.js');
+module.exports.accounts = require('./accounts.js');
 
 
--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,8 @@ const router = require('express').Router();
 router.post('/jobs/addjob', controllers.jobs.postJob);
 router.post('/notes/addnote', controllers.notes.postNote);
 router.post('/notification/addnotification', controllers.notifications.postNotification);
+router.post('/accounts/addaccount', controllers.accounts.postAccount);
+
 
 
 module.exports = router;
