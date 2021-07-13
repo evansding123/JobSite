c17e1a850e8177e1832f36e0994a27577bfedd1a
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-13 00:23:22 -0700

    Added get jobs outline

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -1,11 +1,29 @@
 const pool = require('../../db/index.js');
 
 module.exports = {
-  // --POST
   addJob: async (values = []) => {
     const query = `INSERT INTO jobs (industry, employment_type, title, salary, remote, experience, date, employers_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
     try {
       const res = await pool.query(query, values);
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getOneJob: async (id) => {
+    const query = `SELECT * FROM jobs WHERE id = $1`;
+    try {
+      const res = await pool.query(query, [id]);
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getJobs: async (count = 5, page = 1) => {
+    //filter by certain parameters
+    const query = `SELECT * FROM jobs ORDER BY id LIMIT $1 OFFSET $2`;
+    try {
+      const res = await pool.query(query, [count, page]);
       return res;
     } catch (error) {
       throw error;
