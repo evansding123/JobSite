950385bd9f133a4f70a5b955074c6a61e24b6662
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-12 17:33:27 -0700

    Added post request for jobs

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -3,7 +3,7 @@ const models = require('../models/index.js');
 module.exports = {
   postJob: async (req, res) => {
     try {
-      const queryData = await models.addJob();
+      const queryData = await models.jobs.addJob(req.body);
       res.status(201).send(queryData);
     } catch (error) {
       res.status(404).send(error);
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 const controllers = require('./controllers/index.js');
 const router = require('express').Router();
 
-console.log(controllers);
-router.post('/jobs/:jobpost', controllers.jobs.postJob);
+router.post('/jobs/postnewjob', controllers.jobs.postJob);
 
 module.exports = router;
