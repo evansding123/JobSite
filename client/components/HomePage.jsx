3ef916af854cf3531660e01c43a5861e14c0a02e
John Yasis <johnamielyasis@gmail.com>
2021-07-14 15:43:01 -0700

    some bug fixes on home page not returning anything

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -51,40 +51,23 @@ const HomePage = () => {
         matches.push(jobList[i]);
       }
     }
-    if (matches.length >= 1) {
-      updateListings(matches);
-    } else {
-      updateListings(null)
-    }
+  }
 
-    const search = (searchTerm) => {
-      const jobList = listingsCopy.slice();
-      const noMatches = null;
-      const matches = [];
-
-      for (var i = 0; i < jobList.length; i++) {
-        if (jobList[i].position.toLowerCase().includes(searchTerm.toLowerCase()) || jobList[i].company.toLowerCase().includes(searchTerm.toLowerCase()) || jobList[i].location.toLowerCase().includes(searchTerm.toLowerCase())) {
-          matches.push(jobList[i]);
-        }
-
-        return (
-          <div>
-            {listings === null
-              ? <NoResults onClick={() => { updateListings(listingsCopy) }}>No Results - Return Home</NoResults>
-              : (
-                <ContentContainer>
-                  <Search search={search} />
-                  <JobList listings={listings} />
-                </ContentContainer>
-              )}
-            {/* <Footer>
+  return (
+    <div>
+      {listings === null
+        ? <NoResults onClick={() => { updateListings(listingsCopy) }}>No Results - Return Home</NoResults>
+        : (
+          <ContentContainer>
+            <Search search={search} />
+            <JobList listings={listings} />
+          </ContentContainer>
+        )}
+      {/* <Footer>
           © 2021 JobSite
         </Footer> */}
-          </div>
-        );
-      }
-    }
-  }
+    </div>
+  );
 }
 
 export default HomePage;
