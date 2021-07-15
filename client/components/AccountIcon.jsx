6fbfe67a7ca8254f4180cf646c7ddb923d503702
seanclaybaugh <sdclaybaugh@gmail.com>
2021-07-14 21:02:22 -0700

    implemented AccountIcon menu component with basic functionality, need to add routes to options

--- client/components/AccountIcon.jsx
+++ client/components/AccountIcon.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -11,10 +11,37 @@ const AccountCircle = styled(AccountCircleIcon)`
   margin-right: 1vh;
 `
 
-function AccountIcon () {
-  return (
 
-    <AccountCircle />
+
+function AccountIcon () {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event.currentTarget)
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    console.log('clicked')
+  };
+
+  return (
+    <>
+    <AccountCircle aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} />
+
+  <Menu
+    id="simple-menu"
+    anchorEl={anchorEl}
+    keepMounted
+    open={Boolean(anchorEl)}
+    onClose={handleClose}
+  >
+    <MenuItem onClick={handleClose}>Profile</MenuItem>
+    <MenuItem onClick={handleClose}>My account</MenuItem>
+    <MenuItem onClick={handleClose}>Logout</MenuItem>
+  </Menu>
+  </>
   )
 }
 
