feba891c19ca7db154d94269a1855214dfdebff2
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-15 15:12:20 -0700

    Added notes rendering and used style and css loader

--- client/components/NotesMain.jsx
+++ client/components/NotesMain.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 export default function NotesMain(props) {
   return (
--- client/components/NotesPage.jsx
+++ client/components/NotesPage.jsx
@@ -6,7 +6,6 @@ import NotesMain from './NotesMain.jsx';
 
 const Tester = styled.div`
   display: grid;
-  poistion: relative;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(12, 1fr);
   height: 100vh;
@@ -14,22 +13,43 @@ const Tester = styled.div`
 `;
 
 const Hello = styled.div`
-grid-column: 1;
-grid-row: 1;
+  grid-column: 1;
+  grid-row: 1;
 `;
 
 const List = styled.div`
-grid-column: 1 / 5;
-grid-row: 1 / 12;
-background: #274358;
-border-radius: 10px;
+  grid-column: 1 / 5;
+  grid-row: 1 / 12;
+  background: #192A34;
 `;
 
+const innerList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+`;
+
+const Note = styled.div`
+  font-family: Helvetica;
+  color: #ffffff;
+  grid-column: 2 / 12;
+  margin: 5px;
+  padding: 5px;
+  background: #274358;
+  border-radius: 5px;
+`;
+
+
+
 export default function NotesPage(props) {
+  const notes = Array(10).fill('"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo?')
+  const notesList = notes.map((note) => <Note><p>{note}</p></Note>);
+
   return (
     <Tester>
-      <List>list</List>
-      {/* <Hello>hello</Hello> */}
+      <List>
+        {notesList}
+      </List>
+
       <NotesMain/>
     </Tester>
   )
