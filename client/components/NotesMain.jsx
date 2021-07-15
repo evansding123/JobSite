f096ecf7a8fd1d0b7f1549925bd8bef38c320ad4
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-15 14:45:20 -0700

    Added notes section for notes page

---
+++ client/components/NotesMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+
+export default function NotesMain(props) {
+  return (
+    <div>
+      can you see me?
+    </div>
+  )
+};
--- client/components/NotesPage.jsx
+++ client/components/NotesPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import NotesMain from './NotesMain.jsx';
+
+
+const Tester = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(12, 1fr);
+  height: 110vh;
+  width: 100vh;
+`;
+
+const Hello = styled.div`
+grid-column: 1;
+grid-row: 1;
+`;
+
+const List = styled.div`
+grid-column: 1 / 5;
+grid-row: 1 / 12;
+background: #274358;
+`;
 
 export default function NotesPage(props) {
   return (
-    <div>
-
-    </div>
+    <Tester>
+      <List>list</List>
+      {/* <Hello>hello</Hello> */}
+      <NotesMain/>
+    </Tester>
   )
 };
