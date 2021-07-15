6bac37fcac8c3b8a364aee206b9254ca2974fee4
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-14 21:27:59 -0700

    changed formatting for promo

--- client/components/EmployerPromo.jsx
+++ client/components/EmployerPromo.jsx
@@ -8,6 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 
+const PromoContainer = styled.div`
+  margin: 3%;
+  text-align: center;
+`;
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -32,6 +36,8 @@ const useStyles = makeStyles(() => ({
   },
   paper: {
     backgroundColor: '#363056',
+    margin: ' 2% 4%',
+    padding: '4%',
   }
 }));
 
@@ -39,38 +45,44 @@ export default function EmployerPromo() {
   const classes = useStyles();
 
   return (
-    <div id="employer-promo">
-      <TextContainer>
-          <Typography gutterBottom variant="h3" className={classes.text} >
-            How it Works
+    <PromoContainer>
+        <Typography gutterBottom variant="h3" className={classes.text} >
+          How it Works
+        </Typography>
+      <div id="employer-promo">
+        <TextContainer>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography gutterBottom variant="h6" className={classes.text} >
+              STEP 1: Create Your Job Post
+            </Typography>
+          <Typography gutterBottom variant="body1"className={classes.body}>
+            Once you post your job, it gets sent to matching candidates, then invites them to apply.
           </Typography>
-        <Paper elevation={3} classes={classes.paper}>
-          <Typography gutterBottom variant="h5" className={classes.text} >
-            STEP 1: Create Your Job Post
-          </Typography>
-        <Typography gutterBottom variant="h6"className={classes.body} >
-          Once you post your job, it gets sent to matching candidates, then invites them to apply.
-        </Typography>
-        </Paper>
-        <Typography gutterBottom variant="h5" className={classes.text} >
-          STEP 2: Schedule Interviews
-        </Typography>
-        <Typography gutterBottom variant="h6"className={classes.body} >
-          Use JobSite's calendar tool to schedule phone and video interviews.
-        </Typography>
-        <Typography gutterBottom variant="h5" className={classes.text} >
-          STEP 3: Review Your Great Matches
-        </Typography>
-        <Typography gutterBottom variant="h6"className={classes.body} >
-          As candidates apply, use JobSite's notes tool to organize thoughts and keep track of feedback.
-        </Typography>
-      </TextContainer>
-      <Card raised className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image="/img/notes-tool.jpg"
-        />
-      </Card>
-    </div>
+          </Paper>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h6" className={classes.text} >
+              STEP 2: Schedule Interviews
+            </Typography>
+            <Typography gutterBottom variant="body1"className={classes.body} >
+              Use JobSite's calendar tool to schedule phone and video interviews.
+            </Typography>
+          </Paper>
+          <Paper className={classes.paper}>
+            <Typography gutterBottom variant="h6" className={classes.text} >
+              STEP 3: Review Your Great Matches
+            </Typography>
+            <Typography gutterBottom variant="body1"className={classes.body} >
+              As candidates apply, use JobSite's notes tool to organize thoughts and keep track of feedback.
+            </Typography>
+          </Paper>
+        </TextContainer>
+        <Card raised className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            image="/img/notes-tool.jpg"
+          />
+        </Card>
+      </div>
+    </PromoContainer>
   );
 }
