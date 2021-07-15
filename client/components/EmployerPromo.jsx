9e408d1a8996e7f995fa23f6c936a733bc853aec
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-14 21:56:43 -0700

    Added key

--- client/components/EmployerPromo.jsx
+++ client/components/EmployerPromo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
@@ -39,19 +39,25 @@ const useStyles = makeStyles(() => ({
     margin: ' 2% 4%',
     padding: '4%',
     opacity: '10%',
+    '&hover': {
+      opacity: '100%',
+    },
   }
 }));
 
 const list = [
   {
+    step: 1,
     title:'STEP 1: Create Your Job Post',
     body: ' Once you post your job, it gets sent to matching candidates, then invites them to apply.'
   },
   {
+    step: 2,
     title:'STEP 2: Schedule Interviews',
     body: 'Use JobSite\'s calendar tool to schedule phone and video interviews.'
   },
   {
+    step: 3,
     title:'STEP 3: Review Your Great Matches',
     body: 'As candidates apply, use JobSite\'s notes tool to organize thoughts and keep track of feedback.'
   },
@@ -59,6 +65,7 @@ const list = [
 
 export default function EmployerPromo() {
   const classes = useStyles();
+  const [hover, setHover] = useState(true);
 
   return (
     <div id="employer-promo">
@@ -68,8 +75,8 @@ export default function EmployerPromo() {
 
       <PromoContainer>
         <TextContainer>
-          {list.map(( {title, body }) => (
-            <Paper elevation={5} className={classes.paper}>
+          {list.map(( {step, title, body }) => (
+            <Paper elevation={5} className={classes.paper} key={step}>
               <Typography gutterBottom variant="h6" className={classes.text} >
                 {title}
               </Typography>
