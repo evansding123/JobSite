a432aa58a07c211f70f249e15ed4ad83e334cb90
John Yasis <johnamielyasis@gmail.com>
2021-07-15 12:45:49 -0700

    adds correct jobs fields, makes sure that all fields are filled before submission

--- client/components/CreateJob.jsx
+++ client/components/CreateJob.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import { theme } from '../src/constants';
 
 const Container = styled.div`
@@ -10,7 +11,7 @@ const Container = styled.div`
 `;
 
 const Header = styled.h1`
-  margin: 20px;
+  margin: 10px;
   display: flex;
   justify-content: center;
   color: ${theme.colors.accent};
@@ -23,7 +24,7 @@ const InnerContainer = styled.div`
   width: 20vw;
   margin-top: 80px;
   border-radius: 7px;
-  padding: 80px;
+  padding: 20px 50px 70px 50px;
   box-shadow: 0px 4px 16px rgba(0,0,0,0.2);
   justify-content: center;
   max-height: 560px;
@@ -66,26 +67,36 @@ const TextArea = styled.textarea`
   width: 100%;
 `;
 
+const today = new Date().toString();
+
+/*
+[industry, employment_type, title, salary, remote, experience, date, employers_id]
+*/
+
 const initialForm = {
+  industry: '',
+  employment_type: '',
   title: '',
-  location: '',
-  compensation: '',
+  salary: '',
+  remote: '',
   experience: '',
-  description: '',
+  date: `${today}`,
+  employers_id: ''
 };
 
 export default function CreateJob() {
   const [form, setForm] = useState(initialForm);
 
   function handleChange(e) {
-    let updatedForm = {...form};
+    let updatedForm = { ...form };
     updatedForm[e.target.name] = e.target.value;
     setForm(updatedForm);
   }
 
   function handleSubmit(e) {
-    alert(form); // test for now
+    alert('Job posted!'); // test for now
     setForm(initialForm);
+    console.log(form);
     e.preventDefault();
   }
 
@@ -95,16 +106,38 @@ export default function CreateJob() {
         <InnerContainer>
           <Header>Post a Job</Header>
           <Form onSubmit={handleSubmit}>
-            <Input onChange={handleChange} value={form.title} name="title" type="text" placeholder="Title *" required></Input>
-            <Input onChange={handleChange} value={form.location} name="location" type="text" placeholder="Location *" required></Input>
-            <Input onChange={handleChange} value={form.compensation} name="compensation" type="number" placeholder="Compensation *" required></Input>
-            <Select onChange={handleChange} value={form.experience} name="experience" placeholder="Experience Level *" required>
-              <option value="EntryLevel" default>Entry Level</option>
-              <option value="MidLevel">Mid Level</option>
-              <option value="SeniorLevel">Senior Level</option>
-              <option value="ExecutiveLevel">Executive Level</option>
+            <Select onChange={handleChange} value={form.industry} name="industry" required>
+              <option value="" defaultValue>Select Industry *</option>
+              <option value="Agriculture">Agriculture</option>
+              <option value="Construction" >Construction</option>
+              <option value="Consulting" >Consulting</option>
+              <option value="Education" >Education</option>
+              <option value="Healthcare" >Healthcare</option>
+              <option value="Hospitality" >Hospitality</option>
+              <option value="Law" >Law</option>
+              <option value="Manufacturing" >Manufacturing</option>
+              <option value="Transportation" >Transportation</option>
+              <option value="Tech" >Tech</option>
+              <option value="Utilities" >Utilities</option>
+              <option value="Other" >Other</option>
             </Select>
-            <TextArea onChange={handleChange} value={form.description} name="description" maxLength="1000" placeholder="Description *" required></TextArea>
+            <Select onChange={handleChange} value={form.employment_type} name="employment_type" required>
+              <option value="" defaultValue>Select Employment Type *</option>
+              <option value="Full Time" >Full Time</option>
+              <option value="Part Time">Part Time</option>
+              <option value="Temporary">Temporary</option>
+              <option value="Internship">Internship</option>
+            </Select>
+            <Input onChange={handleChange} value={form.title} name="title" type="text" placeholder="Title *" required></Input>
+            <Input onChange={handleChange} value={form.salary} name="salary" type="number" placeholder="Salary *" required></Input>
+            <Select onChange={handleChange} value={form.experience} name="experience" required>
+              <option value="" defaultValue>Select Experience Level *</option>
+              <option value="Entry" defaultValue>Entry Level</option>
+              <option value="Mid-level">Mid Level</option>
+              <option value="Senior">Senior Level</option>
+              <option value="Executive">Executive Level</option>
+            </Select>
+            {/* <TextArea onChange={handleChange} value={form.description} name="description" maxLength="1000" placeholder="Description *" required></TextArea> */}
             <Submit type="submit"><strong>SUBMIT</strong></Submit>
           </Form>
         </InnerContainer>
