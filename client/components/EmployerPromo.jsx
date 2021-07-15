ff2cb83e9ecb97708cd9a7870708f9cdaeb47f19
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-14 20:35:32 -0700

    increased size of showcase image

--- client/components/EmployerPromo.jsx
+++ client/components/EmployerPromo.jsx
@@ -3,9 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(() => ({
