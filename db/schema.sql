34f7d6ab75a560edd4f6ceff6145be5b25cc52e5
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-15 12:45:31 -0700

    Added notes section and changed queries

--- db/schema.sql
+++ db/schema.sql
@@ -42,8 +42,8 @@ CREATE TYPE experience AS ENUM (
 
 CREATE TABLE "accounts" (
   "id" serial PRIMARY KEY,
-  "username" varchar(255) NOT NULL UNIQUE,
-  "password" varchar(255) NOT NULL,
+  "first_name" varchar(255) NOT NULL,
+  "last_name" varchar(255) NOT NULL,
   "email" varchar(255) NOT NULL UNIQUE,
   "longitude" varchar(255),
   "latitude" varchar(255),
--- server/models/accounts.js
+++ server/models/accounts.js
@@ -2,7 +2,7 @@ const pool = require('../../db/index.js');
 
 module.exports = {
   addAccount: async (values = []) => {
-    const query = `INSERT INTO accounts (email, username, password, longitude, latitude, location_address) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const query = `INSERT INTO accounts (first_name, last_name, email, longitude, latitude, location_address) VALUES ($1, $2, $3, $4, $5, $6)`;
     try {
       const res = await pool.query(query, values);
       return res;
