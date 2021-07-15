275d924caddd86245de5bdc9dbb784a072034980
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-15 02:43:57 -0700

    Added reviews

--- client/components/EmployerPromo.jsx
+++ client/components/EmployerPromo.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
-import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
--- client/components/EmployerReviews.jsx
+++ client/components/EmployerReviews.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import SearchIcon from '@material-ui/icons/Search';
-
 
 const fakeReviews = [
   {
     id: 1,
     review: 'Jobsite makes it so easy to reach a large, targeted candidate pool in a short amount of time',
-    name: 'Evans Ding',
+    name: 'Evans Ding, Software Engineer at Facebook',
+    icon: <FacebookIcon/>,
   },
   {
     id: 2,
     review: 'We have found so many quality candidates on Jobsite than we have on competitor sites',
-    name: 'John Yasis',
+    name: 'John Yasis, Software Engineer at Google',
   },
   {
     id: 3,
     review: 'Jobsite means more than job postings for us. It is their technology and support that really make them a true partner',
-    name: 'Brandon Parker',
+    name: 'Brandon Parker, Software Engineer at Tesla',
   }
 ];
 
-const useStyles = makeStyles(() => ({
-  root: {
+const ReviewContainer = styled.div`
+  color: #FFFFFF;
+  margin: 2% 10%;
+`;
 
+const useStyles = makeStyles(() => ({
+  title: {
+    color: '#FFFFFF',
+    marginBottom:'8vh',
   },
 }));
 
@@ -36,7 +41,19 @@ export default function EmployerReviews() {
 
   return (
     <div id="employer-reviews">
-
+      <Typography gutterBottom variant="h3" align="center" className={classes.title}>
+        Employer Reviews
+      </Typography>
+        {fakeReviews.map(({ id, review, name, icon }) => (
+          <ReviewContainer key={id}>
+            <Typography gutterBottom variant="h4" align="center">
+              {review}
+            </Typography>
+            <Typography gutterBottom variant="h6" align="center">
+              - {name}
+            </Typography>
+          </ReviewContainer>
+        ))}
     </div>
   );
 }
