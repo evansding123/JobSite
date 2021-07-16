75ffc46d1fd986313a76d36353de13b415440352
seanclaybaugh <sdclaybaugh@gmail.com>
2021-07-15 21:08:26 -0700

    tidied up notification component

--- client/components/NotificationIcon.jsx
+++ client/components/NotificationIcon.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Badge from '@material-ui/core/Badge';
+import { Link, useHistory } from "react-router-dom";
 
 const NotificationI = styled(NotificationsIcon)`
   color: white;
   transition: all .2s ease-in-out;
+  padding-bottom: .5vh;
 
   &:hover ${NotificationI} {
     color: #E9EB9E;
@@ -14,15 +16,19 @@ const NotificationI = styled(NotificationsIcon)`
     transform: scale(1.2);
   }
 `
+const IconLink = styled(Link)`
+text-decoration: none;
+`
 
 function NotificationIcon () {
   const [ badgeVisibility, setBadgeVisibility ] = useState(false);
   //
   return (
     <>
-    <Badge variant="dot" color="secondary" invisible={badgeVisibility} overlap="circular">
+    <IconLink to="/calendar">
+    <Badge variant="dot" color="secondary" invisible={badgeVisibility} overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}}>
       <NotificationI onClick={()=> {setBadgeVisibility(true)}}/>
-      </Badge>
+      </Badge></IconLink>
     </>
   )
 }
